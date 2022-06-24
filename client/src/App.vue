04b0d7eb89da6f287035fd161010c1450b257cef
chenjiahau <ivan.chen@dlinkcorp.com>
2022-06-24 14:20:36 +0800

    Draft

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="div">Hi Vue</div>
+  <div>
+    <div id="nav">
+      <router-link to="/" class="nav-item">Home</router-link>
+      <router-link to="/about" class="nav-item">About</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
@@ -10,12 +16,15 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+#nav .nav-item {
+  display: inline-block;
+  padding: 4px;
 }
 </style>
