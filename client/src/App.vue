935af391f195c31de618edaffbb7f0c5a5585082
chenjiahau <ivan.chen@dlinkcorp.com>
2022-07-14 14:53:54 +0800

    Done

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,108 @@
 <template>
-  <div class="div">Hi Vue</div>
+  <div class="content">
+    <div class="operation">
+      <button @click="toggleList" v-if="toggleListFlag">Hide List</button>
+      <button @click="toggleList" v-if="!toggleListFlag">Show List</button>
+      <button @click="add">Add</button>
+      <input type="text" ref="newOne" />
+    </div>
+    <div
+      class="message-list"
+      v-if="toggleListFlag"
+    >
+      <div v-for="message in messageList" :key="message.title">
+        <MessageItem
+          :message="message"
+          @changeOne="changeOne"
+          @deleteOne="deleteOne"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import MessageItem from './components/MessageItem.vue';
+
+const defaultMessageList = [
+  { id: 1, title: 'Hi' },
+  { id: 2, title: 'Welcome' },
+  { id: 3, title: 'Thank you' },
+]
 
 export default {
   name: 'App',
+  components: {
+    MessageItem
+  },
+  data() {
+    return {
+      toggleListFlag: true,
+      messageList: defaultMessageList
+    }
+  },
+  methods: {
+    toggleList() {
+      this.toggleListFlag = !this.toggleListFlag;
+    },
+    add() {
+      this.messageList.push({
+        id: this.messageList.length + 1,
+        title: this.$refs.newOne.value
+      })
+    },
+    changeOne(id, title) {
+      const index = this.messageList.findIndex(message => id === message.id);
+      this.messageList[index].title = title;
+    },
+    deleteOne(id) {
+      this.messageList = this.messageList.filter(message => message.id !== id);
+    }
+  },
+  beforeCreate() {
+    console.log('App-beforeCreate');
+  },
+  created() {
+    console.log('App-created');
+  },
+  beforeMount() {
+    console.log('App-beforeMount');
+  },
+  mounted() {
+    console.log('App-mounted');
+  },
+  beforeUpdate() {
+    console.log('App-beforeUpdate');
+  },
+  updated() {
+    console.log('App-updated');
+  },
+  beforeUnmount() {
+    console.log('App-beforeUnmount');
+  },
+  unmounted() {
+    console.log('App-unmounted');
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 60%;
+  margin: 16px auto;
+}
+
+.operation {
+  flex-direction: row;
+  width: 100%;
+  margin-bottom: 26px;
+}
+
+.message-list {
+  width: 100%;
 }
 </style>
---
+++ client/src/components/MessageItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="message">
+    <div class="title">
+      <input
+        type="text"
+        :value="message.title"
+        ref="existOne"
+      />
+    </div>
+    <div class="action">
+      <button @click="changeOne(message.id)">Change</button>
+      <button @click="deleteOne(message.id)">Delete</button>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'MessageItem',
+  props: ['message'],
+  methods: {
+    changeOne(id) {
+      this.$emit('changeOne', id, this.$refs.existOne.value);
+    },
+    deleteOne(id) {
+      this.$emit('deleteOne', id);
+    }
+  },
+  beforeCreate() {
+    console.log('MessageItem-beforeCreate');
+  },
+  created() {
+    console.log('MessageItem-created');
+  },
+  beforeMount() {
+    console.log('MessageItem-beforeMount');
+  },
+  mounted() {
+    console.log('MessageItem-mounted');
+  },
+  beforeUpdate() {
+    console.log('MessageItem-beforeUpdate');
+  },
+  updated() {
+    console.log('MessageItem-updated');
+  },
+  beforeUnmount() {
+    console.log('MessageItem-beforeUnmount');
+  },
+  unmounted() {
+    console.log('MessageItem-unmounted');
+  }
+}
+</script>
+
+<style scoped>
+.message {
+  display: flex;
+  width: 100%;
+}
+
+.title {
+  flex: 1;
+}
+</style>
